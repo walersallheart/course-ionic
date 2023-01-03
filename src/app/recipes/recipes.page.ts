e41567b5b9d9a143b9f3bd94cf158b6256005e4d
finalmegaman NA <finalmegaman@gmail.com>
2023-01-02 17:16:36 -0800

    76 Managing State with Services.

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { RecipesService } from './recipes.service';
 
 @Component({
   selector: 'app-recipes',
@@ -8,32 +9,11 @@ import { Recipe } from './recipe.model';
 })
 export class RecipesPage implements OnInit {
 
-  constructor() { }
+  constructor(private recipesService: RecipesService) { }
 
-  recipes: Recipe[] = [
-    {
-      id: 'r1',
-      title: 'Schnitzel',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/2/22/Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
-      ingredients: [
-        'French Fries',
-        'Pork Meat',
-        'Salad'
-      ]
-    },
-    {
-      id: 'r2',
-      title: 'Spaghetti',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/9/92/Spaghettoni.jpg',
-      ingredients: [
-        'Spaghetti',
-        'Meat',
-        'Tomatoes'
-      ]
-    },
-  ];
+  recipes: Recipe[] = [];
 
   ngOnInit() {
+    this.recipes = this.recipesService.getAllRecipes();
   }
-
 }
---
+++ src/app/recipes/recipes.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+  private recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Schnitzel',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/2/22/Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
+      ingredients: [
+        'French Fries',
+        'Pork Meat',
+        'Salad'
+      ]
+    },
+    {
+      id: 'r2',
+      title: 'Spaghetti',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/9/92/Spaghettoni.jpg',
+      ingredients: [
+        'Spaghetti',
+        'Meat',
+        'Tomatoes'
+      ]
+    },
+  ];
+
+  
+  getAllRecipes(): Recipe[] {
+    return [...this.recipes];
+  }
+
+  getRecipe(recipeId: string) {
+    return { ...this.recipes.find(recipe => recipe.id === recipeId) };
+  }
+}
